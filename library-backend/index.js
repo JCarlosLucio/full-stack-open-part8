bba335eb1d7a15f5005285be0915c536d6b58c6e
jcLucio <c.lucio3@gmail.com>
2021-07-12 22:36:05 -0500

    Add books by genre exercise (8.5) - Update allBooks Query type w/ optional genre parameter - Update allBooks Query resolver to filter w/ author / genre

--- library-backend/index.js
+++ library-backend/index.js
@@ -88,7 +88,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 
@@ -113,10 +113,16 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) {
-        return books;
+      let filtered = books;
+      if (args.author) {
+        filtered = filtered.filter((book) => book.author === args.author);
       }
-      return books.filter((book) => book.author === args.author);
+
+      if (args.genre) {
+        filtered = filtered.filter((book) => book.genres.includes(args.genre));
+      }
+
+      return filtered;
     },
     allAuthors: () => authors,
   },
