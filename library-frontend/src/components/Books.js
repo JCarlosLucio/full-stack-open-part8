cfc7c7b07663e80b68a22506f4649b8261b26ac4
jcLucio <c.lucio3@gmail.com>
2021-07-16 18:20:50 -0500

    Add books by genre exercise (8.19) - Update ALL_BOOKS query to include genres - Add filter by genre and genre buttons to Books component

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -1,7 +1,9 @@
 import { useQuery } from '@apollo/client';
+import { useState } from 'react';
 import { ALL_BOOKS } from '../queries';
 
 const Books = ({ show }) => {
+  const [filter, setFilter] = useState(null);
   const result = useQuery(ALL_BOOKS);
 
   if (!show) {
@@ -13,19 +15,26 @@ const Books = ({ show }) => {
   }
 
   const books = result.data.allBooks;
+  const genres = [...new Set(books.flatMap((book) => book.genres))];
+
+  const filtered = filter
+    ? books.filter((book) => book.genres.includes(filter))
+    : books;
 
   return (
     <div>
       <h2>books</h2>
-
+      <p>
+        in genre <strong>{filter ? filter : 'all genres'}</strong>
+      </p>
       <table>
         <tbody>
           <tr>
-            <th></th>
+            <th>title</th>
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {filtered.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -34,6 +43,14 @@ const Books = ({ show }) => {
           ))}
         </tbody>
       </table>
+      <div>
+        {genres.map((genre) => (
+          <button key={genre} onClick={() => setFilter(genre)}>
+            {genre}
+          </button>
+        ))}
+        <button onClick={() => setFilter(null)}>all genres</button>
+      </div>
     </div>
   );
 };
