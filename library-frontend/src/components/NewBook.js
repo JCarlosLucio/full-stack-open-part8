1a837c1ae7cb2db052c15d985b9201375be04ccb
jcLucio <c.lucio3@gmail.com>
2021-07-22 16:41:09 -0500

    Add Up to date cache and book recommendations exercise (8.22) - Update CREATE_BOOK useMutation w/update

--- library-frontend/src/components/NewBook.js
+++ library-frontend/src/components/NewBook.js
@@ -1,7 +1,13 @@
 import { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import PropTypes from 'prop-types';
-import { ALL_AUTHORS, ALL_BOOKS, CREATE_BOOK } from '../queries';
+import {
+  ALL_AUTHORS,
+  ALL_BOOKS,
+  BOOKS_BY_GENRE,
+  CREATE_BOOK,
+  ME,
+} from '../queries';
 
 const NewBook = ({ show, setPage }) => {
   const [title, setTitle] = useState('');
@@ -11,7 +17,47 @@ const NewBook = ({ show, setPage }) => {
   const [genres, setGenres] = useState([]);
 
   const [createBook] = useMutation(CREATE_BOOK, {
-    refetchQueries: [{ query: ALL_AUTHORS }, { query: ALL_BOOKS }],
+    update: (store, response) => {
+      const authorsInStore = store.readQuery({ query: ALL_AUTHORS });
+      const booksInStore = store.readQuery({ query: ALL_BOOKS });
+      const meInStore = store.readQuery({ query: ME });
+      const booksByGenreInStore = store.readQuery({
+        query: BOOKS_BY_GENRE,
+        variables: { genre: meInStore.me.favoriteGenre },
+      });
+
+      // update bookCount of author
+      store.writeQuery({
+        query: ALL_AUTHORS,
+        data: {
+          ...authorsInStore,
+          allAuthors: authorsInStore.allAuthors.map((a) =>
+            a.name === author ? { ...a, bookCount: a.bookCount + 1 } : a
+          ),
+        },
+      });
+
+      // update books list with addedBook
+      store.writeQuery({
+        query: ALL_BOOKS,
+        data: {
+          ...booksInStore,
+          allBooks: [...booksInStore.allBooks, response.data.addBook],
+        },
+      });
+
+      // update recommended book list if book is from favorite genre
+      if (genres.includes(meInStore.me.favoriteGenre)) {
+        store.writeQuery({
+          query: BOOKS_BY_GENRE,
+          variables: { genre: meInStore.me.favoriteGenre },
+          data: {
+            ...booksByGenreInStore,
+            allBooks: [...booksByGenreInStore.allBooks, response.data.addBook],
+          },
+        });
+      }
+    },
   });
 
   if (!show) {
